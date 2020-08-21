b357365c98b2efeeccdd91ac478c5365aa44c023
Tom <tom@tomdg.com>
2020-08-21 05:41:38 +0100

    Refactor: create a generic tile entity base class for machines.

--- src/main/java/com/quantumshark/testmod/tileentity/ExampleFurnaceTileEntity.java
+++ src/main/java/com/quantumshark/testmod/tileentity/ExampleFurnaceTileEntity.java
@@ -1,67 +1,39 @@
 package com.quantumshark.testmod.tileentity;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-import javax.annotation.Nullable;
-
 import com.quantumshark.testmod.TestMod;
 import com.quantumshark.testmod.blocks.ExampleFurnaceBlock;
 import com.quantumshark.testmod.container.ExampleFurnaceContainer;
 import com.quantumshark.testmod.recipes.ExampleRecipe;
-import com.quantumshark.testmod.utill.ExampleItemHandler;
-import com.quantumshark.testmod.utill.RecipeSerializerInit;
 import com.quantumshark.testmod.utill.RegistryHandler;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.inventory.container.Container;
-import net.minecraft.inventory.container.INamedContainerProvider;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.item.crafting.IRecipeType;
-import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.play.server.SUpdateTileEntityPacket;
-import net.minecraft.tileentity.ITickableTileEntity;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
-import net.minecraft.util.Direction;
-import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.util.Constants;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.CapabilityItemHandler;
-import net.minecraftforge.items.IItemHandlerModifiable;
-import net.minecraftforge.items.wrapper.RecipeWrapper;
 
-public class ExampleFurnaceTileEntity extends TileEntity implements ITickableTileEntity, INamedContainerProvider {
+public class ExampleFurnaceTileEntity extends MachineTileEntityBase {
 
-	private ITextComponent customName;
 	public int currentSmeltTime;
 	public final int maxSmeltTime = 100;
-	private ExampleItemHandler inventory;
 
 	public ExampleFurnaceTileEntity(TileEntityType<?> tileEntityTypeIn) {
 		super(tileEntityTypeIn);
-
-		this.inventory = new ExampleItemHandler(2);
 	}
 
 	public ExampleFurnaceTileEntity() {
 		this(RegistryHandler.EXAMPLE_FURNACE_TILE_ENTITY.get());
 	}
+	
+	@Override
+	public int getInputSlotCount() { return 1; }
+	
+	@Override
+	public int getOutputSlotCount()  { return 1; }
 
 	@Override
 	public Container createMenu(final int windowID, final PlayerInventory playerInv, final PlayerEntity playerIn) {
@@ -74,18 +46,20 @@ public class ExampleFurnaceTileEntity extends TileEntity implements ITickableTil
 
 		if (this.world != null && !this.world.isRemote) {
 			if (this.world.isBlockPowered(this.getPos())) {
-				ItemStack inputStack = this.inventory.getStackInSlot(0);
-				// reset progess if you remove the input item. 
+				ItemStack inputStack = getInputStack(0);
+				// reset progress if you remove the input item. 
 				if(inputStack == null || inputStack.isEmpty())
 				{
 					this.currentSmeltTime = 0;
+					this.world.setBlockState(this.getPos(),
+							this.getBlockState().with(ExampleFurnaceBlock.LIT, false));
 				}
 				else
 				{
 					ExampleRecipe recipe =this.getRecipe(inputStack);
 					if(recipe != null)
 					{
-						ItemStack outputStack = this.inventory.getStackInSlot(1);
+						ItemStack outputStack = getOutputStack(0);
 						if(outputStack == null || outputStack.isEmpty() || canCombine(outputStack, recipe.getRecipeOutput()))
 						{
 							if (this.currentSmeltTime != this.maxSmeltTime) {
@@ -97,9 +71,9 @@ public class ExampleFurnaceTileEntity extends TileEntity implements ITickableTil
 								this.world.setBlockState(this.getPos(),
 										this.getBlockState().with(ExampleFurnaceBlock.LIT, false));
 								this.currentSmeltTime = 0;
-								ItemStack output = this.getRecipe(this.inventory.getStackInSlot(0)).getRecipeOutput();
-								this.inventory.insertItem(1, output.copy(), false);
-								this.inventory.decrStackSize(0, 1);
+								
+								ItemStack output = processRecipe(this.getRecipe(getInputStack(0)));
+								
 								dirty = true;
 							}
 						}
@@ -115,55 +89,14 @@ public class ExampleFurnaceTileEntity extends TileEntity implements ITickableTil
 		}
 	}
 	
-	// todo: make this very public.
-	public static boolean canCombine(ItemStack stack1, ItemStack stack2)
-	{
-		if(stack1 == null || stack2 == null)
-		{
-			// can shove anything in an empty slot
-			return true;
-		}
-		// check types, tags ...
-		if(!ItemStack.areItemsEqual(stack1,  stack2))
-		{
-			return false;
-		}
-		// check for overflow
-		return (stack1.getCount() + stack2.getCount() <= stack1.getMaxStackSize());
-	}
-
-	public void setCustomName(ITextComponent name) {
-		this.customName = name;
-	}
-
-	public ITextComponent getName() {
-		return this.customName != null ? this.customName : this.getDefaultName();
-	}
-
-	private ITextComponent getDefaultName() {
-		return new TranslationTextComponent("container." + TestMod.MOD_ID + ".example_furnace");
-	}
-
 	@Override
-	public ITextComponent getDisplayName() {
-		return this.getName();
-	}
-
-	@Nullable
-	public ITextComponent getCustomName() {
-		return this.customName;
+	protected ITextComponent getDefaultName() {
+		return new TranslationTextComponent("container." + TestMod.MOD_ID + ".example_furnace");
 	}
 
 	@Override
 	public void read(CompoundNBT compound) {
 		super.read(compound);
-		if (compound.contains("CustomName", Constants.NBT.TAG_STRING)) {
-			this.customName = ITextComponent.Serializer.fromJson(compound.getString("CustomName"));
-		}
-
-		NonNullList<ItemStack> inv = NonNullList.<ItemStack>withSize(this.inventory.getSlots(), ItemStack.EMPTY);
-		ItemStackHelper.loadAllItems(compound, inv);
-		this.inventory.setNonNullList(inv);
 
 		this.currentSmeltTime = compound.getInt("CurrentSmeltTime");
 	}
@@ -171,115 +104,8 @@ public class ExampleFurnaceTileEntity extends TileEntity implements ITickableTil
 	@Override
 	public CompoundNBT write(CompoundNBT compound) {
 		super.write(compound);
-		if (this.customName != null) {
-			compound.putString("CustomName", ITextComponent.Serializer.toJson(this.customName));
-		}
-
-		ItemStackHelper.saveAllItems(compound, this.inventory.toNonNullList());
 		compound.putInt("CurrentSmeltTime", this.currentSmeltTime);
 
 		return compound;
-	}
-
-	@Nullable
-	public ExampleRecipe getRecipe(ItemStack stack) {
-		if (stack == null) {
-			return null;
-		}
-
-		Set<IRecipe<?>> recipes = findRecipesByType(RecipeSerializerInit.EXAMPLE_TYPE, this.world);
-		for (IRecipe<?> iRecipe : recipes) {
-			ExampleRecipe recipe = (ExampleRecipe) iRecipe;
-			if (recipe.matches(new RecipeWrapper(this.inventory), this.world)) {
-				return recipe;
-			}
-		}
-
-		return null;
-	}
-	
-	public boolean hasRecipe(ItemStack stack) {
-		if (stack == null) {
-			return false;
-		}
-	
-		Set<IRecipe<?>> recipes = findRecipesByType(RecipeSerializerInit.EXAMPLE_TYPE, this.world);
-		for (IRecipe<?> iRecipe : recipes) {
-			ExampleRecipe recipe = (ExampleRecipe) iRecipe;
-			for(Ingredient ing : recipe.getIngredients()) {
-				if(ing.test(stack)) {
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
-	public static Set<IRecipe<?>> findRecipesByType(IRecipeType<?> typeIn, World world) {
-		if(world == null)
-		{
-			return Collections.emptySet();
-		}
-		Set<IRecipeType<?>> recipeTypes = world.getRecipeManager().getRecipes().stream().map(x->x.getType()).distinct().collect(Collectors.toSet());
-		
-		return world.getRecipeManager().getRecipes().stream()
-				.filter(recipe -> recipe.getType() == typeIn).collect(Collectors.toSet());
-	}
-
-	@SuppressWarnings("resource")
-	@OnlyIn(Dist.CLIENT)
-	public static Set<IRecipe<?>> findRecipesByType(IRecipeType<?> typeIn) {
-		ClientWorld world = Minecraft.getInstance().world;
-		return world != null ? world.getRecipeManager().getRecipes().stream()
-				.filter(recipe -> recipe.getType() == typeIn).collect(Collectors.toSet()) : Collections.emptySet();
-	}
-
-	public static Set<ItemStack> getAllRecipeInputs(IRecipeType<?> typeIn, World worldIn) {
-		Set<ItemStack> inputs = new HashSet<ItemStack>();
-		Set<IRecipe<?>> recipes = findRecipesByType(typeIn, worldIn);
-		for (IRecipe<?> recipe : recipes) {
-			NonNullList<Ingredient> ingredients = recipe.getIngredients();
-			ingredients.forEach(ingredient -> {
-				for (ItemStack stack : ingredient.getMatchingStacks()) {
-					inputs.add(stack);
-				}
-			});
-		}
-		return inputs;
-	}
-
-	public final IItemHandlerModifiable getInventory() {
-		return this.inventory;
-	}
-
-	@Nullable
-	@Override
-	public SUpdateTileEntityPacket getUpdatePacket() {
-		CompoundNBT nbt = new CompoundNBT();
-		this.write(nbt);
-		return new SUpdateTileEntityPacket(this.pos, 0, nbt);
-	}
-
-	@Override
-	public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
-		this.read(pkt.getNbtCompound());
-	}
-
-	@Override
-	public CompoundNBT getUpdateTag() {
-		CompoundNBT nbt = new CompoundNBT();
-		this.write(nbt);
-		return nbt;
-	}
-
-	@Override
-	public void handleUpdateTag(CompoundNBT nbt) {
-		this.read(nbt);
-	}
-
-	@Override
-	public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
-		return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.orEmpty(cap, LazyOptional.of(() -> this.inventory));
 	}
 }
---
+++ src/main/java/com/quantumshark/testmod/tileentity/MachineTileEntityBase.java
@@ -0,0 +1,244 @@
+package com.quantumshark.testmod.tileentity;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nullable;
+
+import com.quantumshark.testmod.recipes.ExampleRecipe;
+import com.quantumshark.testmod.utill.ExampleItemHandler;
+import com.quantumshark.testmod.utill.RecipeSerializerInit;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.inventory.ItemStackHelper;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.IRecipeType;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SUpdateTileEntityPacket;
+import net.minecraft.tileentity.ITickableTileEntity;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.Direction;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.wrapper.RecipeWrapper;
+
+// base class for machines
+public abstract class MachineTileEntityBase extends TileEntity implements ITickableTileEntity, INamedContainerProvider {
+	private ExampleItemHandler inventory;
+	private ITextComponent customName;
+
+	public MachineTileEntityBase(TileEntityType<?> tileEntityTypeIn) {
+		super(tileEntityTypeIn);
+
+		this.inventory = new ExampleItemHandler(getInputSlotCount() + getOutputSlotCount());
+	}
+	
+	public abstract int getInputSlotCount();
+	public abstract int getOutputSlotCount();
+	
+	public ItemStack getInputStack(int index) {
+		if(index <0 || index >= getInputSlotCount())
+		{
+			return null;
+		}
+		return inventory.getStackInSlot(index);
+	}
+	
+	public ItemStack getOutputStack(int index) {
+		if(index <0 || index >= getOutputSlotCount())
+		{
+			return null;
+		}
+		return inventory.getStackInSlot(getInputSlotCount() + index);
+	}
+
+	protected ItemStack processRecipe(ExampleRecipe recipe) {
+		// todo: make this handle more complex recipes.
+		// use a recipe interface.
+		ItemStack output = recipe.getRecipeOutput();
+		this.inventory.insertItem(1, output.copy(), false);
+		this.inventory.decrStackSize(0, 1);
+	
+		return output;
+	}
+	
+	public void setCustomName(ITextComponent name) {
+		this.customName = name;
+	}
+
+	public ITextComponent getName() {
+		return this.customName != null ? this.customName : this.getDefaultName();
+	}
+
+	protected abstract ITextComponent getDefaultName();
+
+	@Override
+	public ITextComponent getDisplayName() {
+		return this.getName();
+	}
+
+	@Nullable
+	public ITextComponent getCustomName() {
+		return this.customName;
+	}	
+	
+	@Override
+	public void read(CompoundNBT compound) {
+		super.read(compound);
+		if (compound.contains("CustomName", Constants.NBT.TAG_STRING)) {
+			this.customName = ITextComponent.Serializer.fromJson(compound.getString("CustomName"));
+		}
+
+		NonNullList<ItemStack> inv = NonNullList.<ItemStack>withSize(this.inventory.getSlots(), ItemStack.EMPTY);
+		ItemStackHelper.loadAllItems(compound, inv);
+		this.inventory.setNonNullList(inv);
+	}
+
+	@Override
+	public CompoundNBT write(CompoundNBT compound) {
+		super.write(compound);
+		if (this.customName != null) {
+			compound.putString("CustomName", ITextComponent.Serializer.toJson(this.customName));
+		}
+
+		ItemStackHelper.saveAllItems(compound, this.inventory.toNonNullList());
+
+		return compound;
+	}	
+	
+
+	public final IItemHandlerModifiable getInventory() {
+		return this.inventory;
+	}
+	
+	@Nullable
+	public ExampleRecipe getRecipe(ItemStack stack) {
+		if (stack == null) {
+			return null;
+		}
+
+		Set<IRecipe<?>> recipes = findRecipesByType(RecipeSerializerInit.EXAMPLE_TYPE, this.world);
+		for (IRecipe<?> iRecipe : recipes) {
+			ExampleRecipe recipe = (ExampleRecipe) iRecipe;
+			if (recipe.matches(new RecipeWrapper(this.inventory), this.world)) {
+				return recipe;
+			}
+		}
+
+		return null;
+	}
+	
+	public boolean hasRecipe(ItemStack stack) {
+		if (stack == null) {
+			return false;
+		}
+	
+		Set<IRecipe<?>> recipes = findRecipesByType(RecipeSerializerInit.EXAMPLE_TYPE, this.world);
+		for (IRecipe<?> iRecipe : recipes) {
+			ExampleRecipe recipe = (ExampleRecipe) iRecipe;
+			for(Ingredient ing : recipe.getIngredients()) {
+				if(ing.test(stack)) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public static Set<IRecipe<?>> findRecipesByType(IRecipeType<?> typeIn, World world) {
+		if(world == null)
+		{
+			return Collections.emptySet();
+		}
+		Set<IRecipeType<?>> recipeTypes = world.getRecipeManager().getRecipes().stream().map(x->x.getType()).distinct().collect(Collectors.toSet());
+		
+		return world.getRecipeManager().getRecipes().stream()
+				.filter(recipe -> recipe.getType() == typeIn).collect(Collectors.toSet());
+	}
+
+	@SuppressWarnings("resource")
+	@OnlyIn(Dist.CLIENT)
+	public static Set<IRecipe<?>> findRecipesByType(IRecipeType<?> typeIn) {
+		ClientWorld world = Minecraft.getInstance().world;
+		return world != null ? world.getRecipeManager().getRecipes().stream()
+				.filter(recipe -> recipe.getType() == typeIn).collect(Collectors.toSet()) : Collections.emptySet();
+	}
+
+	public static Set<ItemStack> getAllRecipeInputs(IRecipeType<?> typeIn, World worldIn) {
+		Set<ItemStack> inputs = new HashSet<ItemStack>();
+		Set<IRecipe<?>> recipes = findRecipesByType(typeIn, worldIn);
+		for (IRecipe<?> recipe : recipes) {
+			NonNullList<Ingredient> ingredients = recipe.getIngredients();
+			ingredients.forEach(ingredient -> {
+				for (ItemStack stack : ingredient.getMatchingStacks()) {
+					inputs.add(stack);
+				}
+			});
+		}
+		return inputs;
+	}
+	
+	// todo: make this very public.
+	public static boolean canCombine(ItemStack stack1, ItemStack stack2)
+	{
+		if(stack1 == null || stack2 == null)
+		{
+			// can shove anything in an empty slot
+			return true;
+		}
+		// check types, tags ...
+		if(!ItemStack.areItemsEqual(stack1,  stack2))
+		{
+			return false;
+		}
+		// check for overflow
+		return (stack1.getCount() + stack2.getCount() <= stack1.getMaxStackSize());
+	}
+
+	@Nullable
+	@Override
+	public SUpdateTileEntityPacket getUpdatePacket() {
+		CompoundNBT nbt = new CompoundNBT();
+		this.write(nbt);
+		return new SUpdateTileEntityPacket(this.pos, 0, nbt);
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
+		this.read(pkt.getNbtCompound());
+	}
+
+	@Override
+	public CompoundNBT getUpdateTag() {
+		CompoundNBT nbt = new CompoundNBT();
+		this.write(nbt);
+		return nbt;
+	}
+
+	@Override
+	public void handleUpdateTag(CompoundNBT nbt) {
+		this.read(nbt);
+	}
+
+	@Override
+	public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
+		return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.orEmpty(cap, LazyOptional.of(() -> this.inventory));
+	}
+}
