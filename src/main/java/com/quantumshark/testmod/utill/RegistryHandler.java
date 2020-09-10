2d053928aa3968531599a366f6c76ee396df530f
Quantumshark <thedup.adg@gmail.com>
2020-09-10 20:18:03 +0100

    added blast furnace housing

--- src/main/java/com/quantumshark/testmod/utill/RegistryHandler.java
+++ src/main/java/com/quantumshark/testmod/utill/RegistryHandler.java
@@ -98,6 +98,7 @@ public class RegistryHandler {
 	
 	// Blocks
 	public static final RegistryObject<Block> BLASTBRICKS_BLOCK = BLOCKS.register("blast_bricks", BlastBricksBlock::new);
+	public static final RegistryObject<Block> BLASTFURNACEHOUSING_BLOCK = BLOCKS.register("blast_furnace_housing", BlastBricksBlock::new);
 	public static final RegistryObject<Block> BLUECRYSTALBLOCK_BLOCK = BLOCKS.register("blue_crystal_block", BlueCrystalBlock::new);
 	public static final RegistryObject<Block> BLUECRYSTALORE_BLOCK = BLOCKS.register("blue_crystal_ore", BlueCrystalOre::new);
 	public static final RegistryObject<Block> FLUORITEBLOCK_BLOCK = BLOCKS.register("fluorite_block", FluoriteBlock::new);
@@ -117,6 +118,7 @@ public class RegistryHandler {
 	
 	// Block Items
 	public static final RegistryObject<Item> BLASTBRICKS_BLOCK_ITEM = ITEMS.register("blast_bricks", () -> new BlockItemBase(BLASTBRICKS_BLOCK.get()));
+	public static final RegistryObject<Item> BLASTFURNACEHOUSING_BLOCK_ITEM = ITEMS.register("blast_furnace_housing", () -> new BlockItemBase(BLASTFURNACEHOUSING_BLOCK.get()));
 	public static final RegistryObject<Item> BLUECRYSTALBLOCK_BLOCK_ITEM = ITEMS.register("blue_crystal_block", () -> new BlockItemBase(BLUECRYSTALBLOCK_BLOCK.get()));
 	public static final RegistryObject<Item> BLUECRYSTALORE_BLOCK_ITEM = ITEMS.register("blue_crystal_ore", () -> new BlockItemBase(BLUECRYSTALORE_BLOCK.get()));
 	public static final RegistryObject<Item> FLUORITEBLOCK_BLOCK_ITEM = ITEMS.register("fluorite_block", () -> new BlockItemBase(FLUORITEBLOCK_BLOCK.get()));
